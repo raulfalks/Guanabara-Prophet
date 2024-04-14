149fd96a56a300f6700a4a88271dd702c8a2c70e
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-04-14 20:40:24 -0300

    Modulo F - Exercicio pt 2

--- Exercicio F - 1/script.js
+++ Exercicio F - 1/script.js
@@ -20,9 +20,43 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    if (isNumero(num.value) && !inlista.includes(num.value)) {
-        lista.innerHTML += `<option value="${num.value}">${num.value}</option>`
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `${num.value}`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length ==0) {
+        window.alert('Nenhum valor foi adicionado')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+            
+        }
+        media = soma / tot
+        res.innetHTML= ''
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} nmeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+
     }
 }
