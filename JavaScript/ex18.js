0374e599a41123252e03650dcd7cb237d12eba59
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-04-14 20:22:30 -0300

    Modulo F - Aula 16

---
+++ JavaScript/ex18.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+var res = parimpar(10)
+console.log(res)
+
---
+++ JavaScript/ex20.js
@@ -0,0 +1,6 @@
+var v = function(x, y) {
+    return x * y
+}
+
+console.log(v(2, 3))
+
